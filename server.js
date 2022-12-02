f92e26cc0a6fe50747b81b1ddec06de6304d7304
charlesb9 <charles.bourneuf@gmail.com>
2022-12-02 15:56:53 +0100

    refactor

--- server.js
+++ server.js
@@ -1,64 +1,45 @@
-const http = require('http');
-const express = require('express');
-const app = express();
+import express from 'express'
+import fetch from 'node-fetch'
+
+const app = express()
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    next();
-
-  });
-
-app.use('/api/ban/:filter/', (req, res, next) => {
-
-  let query = req.query.q
-  let arg = query.split(' ').join('+')
-  let filter = req.params.filter
-  new Promise((resolve) => {
-      
-      const http = require('http');    
-      http.get(`http://api-adresse.data.gouv.fr/search/?q=${filter}+${arg}`, (res) => {
-      
-          res.setEncoding('utf8');
-          let data = ''
-          res.on('data', (chunk) => { data += chunk; });
-          res.on('end', () => {
-              resolve(data)
-          })
-  
-      })   
-  })  .then(response => {
-              res.status(200).json(response)
-          })
-      .catch(error => {
-              res.status(404).json({error: error})
-          });
-  
-  });
-
-const normalizePort = val => {
-    const port = parseInt(val, 10);
-
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-};
-
-const port = normalizePort(process.env.PORT || '3000');
-
-app.set('port', port);
-
-const server = http.createServer(app);
-
-server.on('listening', () => {
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log('Listening on ' + bind);
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+	res.setHeader('Access-Control-Allow-Methods', 'GET')
+	next();
 });
 
-server.listen(port);
+app.use('/filter/:filter/', async (req, res, next) => {
+	let query = req.query.q
+	let arg = query.split(' ').join('+')
+	let filter = req.params.filter
+	try {
+		let getData = await fetch(`http://api-adresse.data.gouv.fr/search/?q=${filter}+${arg}`)
+		let data = await getData.json()
+		res.status(200).json(data)
+	} catch (err) {
+		console.log(err)
+		return res.status(404).json(err)
+	}
+});
+
+app.use('', (req, res) => {
+	res.status(404).json({message : 'url format : /filter/<filter>?q=<arguments>'})
+})
+
+const normalizePort = val => {
+	const port = parseInt(val, 10)
+	if (isNaN(port)) {
+		return val
+	}
+	if (port >= 0) {
+		return port
+	}
+	return false
+};
+
+const port = normalizePort(process.env.PORT || '3000')
+app.listen(port, () => {
+	console.log('listen to port : ' + port)
+})
